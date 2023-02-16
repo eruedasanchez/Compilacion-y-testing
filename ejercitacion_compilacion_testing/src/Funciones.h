b46c761dc16a772a9f437836d68a7f5130fdb455
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-16 13:44:27 -0300

    Header de la clase Periodo

---
+++ ejercitacion_compilacion_testing/src/Funciones.h
@@ -0,0 +1,8 @@
+//
+// Created by Ezequiel Rueda Sanchez on 16/02/2023.
+//
+
+#ifndef SOLUCION_FUNCIONES_H
+#define SOLUCION_FUNCIONES_H
+
+#endif //SOLUCION_FUNCIONES_H
---
+++ ejercitacion_compilacion_testing/src/Meses.h
@@ -0,0 +1,8 @@
+//
+// Created by Ezequiel Rueda Sanchez on 16/02/2023.
+//
+
+#ifndef SOLUCION_MESES_H
+#define SOLUCION_MESES_H
+
+#endif //SOLUCION_MESES_H
