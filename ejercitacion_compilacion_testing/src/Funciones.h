7e920696451d48fcbec5065ab2a0c4d445198b00
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-16 13:47:01 -0300

    Header de la clase Funciones

--- ejercitacion_compilacion_testing/src/Funciones.h
+++ ejercitacion_compilacion_testing/src/Funciones.h
@@ -1,8 +1,10 @@
-//
-// Created by Ezequiel Rueda Sanchez on 16/02/2023.
-//
-
 #ifndef SOLUCION_FUNCIONES_H
 #define SOLUCION_FUNCIONES_H
 
-#endif //SOLUCION_FUNCIONES_H
+#include "Meses.h"
+
+bool esBisiesto(Anio anio);
+int diasEnMes(int anio, int mes);
+
+
+#endif
