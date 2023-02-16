24bec7f9cf8d01f52b42a120970d73c5b934dbdd
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-16 14:31:08 -0300

    Casos de tests resueltos hasta ej7

--- ejercitacion_compilacion_testing/tests/ej_testing.cpp
+++ ejercitacion_compilacion_testing/tests/ej_testing.cpp
@@ -6,12 +6,41 @@
 
 using namespace std;
 
-// Ejercicio 4
+/* Ejercicio 4 */
 TEST(Aritmetica, suma) {
+    int calculado = 15 + 7;
+    int esperado = 22;
+    EXPECT_EQ(calculado, esperado);
 }
 
-// Ejercicio 5
-TEST( ... ) {
+/* Ejercicio 5 */
+TEST(Aritmetica, potencia){
+    float potencia = pow(10,2);
+    int esperado = 100;
+    EXPECT_EQ(potencia, esperado);
 }
 
-// Ejercicios 6..9
+/* Ejercicio 6 */
+TEST(Diccionario, obtener){
+    map<int, int> dicc = map<int, int>({{1, 7}, {2, 5}, {3, 8}});
+    EXPECT_EQ(dicc[2], 5);
+
+}
+
+/* Ejercicio 7 */
+TEST(Diccionario, definir){
+    map<int, int> dicc = map<int, int>({{1, 7}, {2, 5}, {3, 8}});
+    int definidaClaveOcho = dicc.count(8);
+    EXPECT_EQ(definidaClaveOcho, 0);
+    /* Ahora defino la clave 8 con su valor asociado */
+    dicc[8] = 4;
+    definidaClaveOcho = dicc.count(8);
+    EXPECT_EQ(definidaClaveOcho, 1);
+}
+
+
+
+
+/* Ejercicio 8 */
+
+/* Ejercicios 9 */
