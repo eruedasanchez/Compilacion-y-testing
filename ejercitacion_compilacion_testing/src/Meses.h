49efb6c364a49a748106ec9601cf12f2a00e9404
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-16 13:46:22 -0300

    Header de la clase Meses

--- ejercitacion_compilacion_testing/src/Meses.h
+++ ejercitacion_compilacion_testing/src/Meses.h
@@ -1,8 +1,23 @@
-//
-// Created by Ezequiel Rueda Sanchez on 16/02/2023.
-//
-
 #ifndef SOLUCION_MESES_H
 #define SOLUCION_MESES_H
 
-#endif //SOLUCION_MESES_H
+// Typedef
+typedef int Anio;
+typedef int Mes;
+typedef int Dia;
+
+// MESES
+const Mes ENERO = 1;
+const Mes FEBRERO = 2;
+const Mes MARZO = 3;
+const Mes ABRIL = 4;
+const Mes MAYO = 5;
+const Mes JUNIO = 6;
+const Mes JULIO = 7;
+const Mes AGOSTO = 8;
+const Mes SEPTIEMBRE = 9;
+const Mes OCTUBRE = 10;
+const Mes NOVIEMBRE = 11;
+const Mes DICIEMBRE = 12;
+
+#endif
